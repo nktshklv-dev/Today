76ab5ae9637d8252eb608f54f0c46f2d738ebe36
nktshklv <nikitashakalov@gmail.com>
2023-01-25 00:50:47 +0300

    created an enumeration for rows

---
+++ Today/DetailViewController/ReminderViewController+Row.swift
@@ -0,0 +1,43 @@
+//
+//  ReminderViewController+Row.swift
+//  Today
+//
+//  Created by Nikita  on 1/25/23.
+//
+
+import Foundation
+import UIKit
+
+extension ReminderViewController {
+    enum Row: Hashable {
+        case viewDate
+        case viewNotes
+        case viewTime
+        case viewTitle
+        
+        var imageName: String? {
+            switch self {
+            case .viewDate: return  "calendar.circle"
+            case .viewNotes: return "square.and.pencil"
+            case .viewTime: return "clock"
+            default: return nil
+            }
+        }
+        
+        var image: UIImage? {
+            guard let imageName = imageName else {return nil}
+            let configuration = UIImage.SymbolConfiguration(textStyle: .headline)
+            return UIImage(systemName: imageName, withConfiguration: configuration)
+        }
+        
+        var textStyle: UIFont.TextStyle {
+            switch self {
+            case .viewTitle: return .headline
+            default: return .subheadline
+            }
+        }
+    }
+    
+  
+    
+}
--- Today/DetailViewController/ReminderViewController.swift
+++ Today/DetailViewController/ReminderViewController.swift
@@ -13,14 +13,23 @@ class ReminderViewController: UICollectionViewController{
     
     init(reminder: Reminder) {
         self.reminder = reminder
+        
         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
         listConfiguration.showsSeparators = false
         let listLayout = UICollectionViewCompositionalLayout.list(using:  listConfiguration)
         super.init(collectionViewLayout: listLayout)
-        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func text(for row: Row) -> String? {
+        switch row{
+        case .viewDate: return reminder.dueDate.dayText
+        case .viewNotes: return reminder.notes
+        case .viewTime: return reminder.dueDate.formatted(date: .omitted, time: .shortened)
+        case .viewTitle: return reminder.title
+        }
     }
 }
